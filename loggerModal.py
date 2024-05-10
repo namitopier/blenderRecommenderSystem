7443c2f1c8ddffbda45df00e6248b26c546eb884
namitopier <pierluigi0609@gmail.com>
2024-05-10 08:24:41 -0300

    set default value for tolerance to 10%

--- loggerModal.py
+++ loggerModal.py
@@ -1041,7 +1041,7 @@ def getPerformedOperations(mouse_x = 0, mouse_y = 0):
 
     return clipboard.split("\n")[:-1] # Has to exclude the last one since it will be "" 
 
-def getFilteredOp (translatedOp, additionalInfo = {}):
+def getFilteredOp (translatedOp, additionalInfo = {"tolerance": 10}):
     # Filters automatically the important values to track + additional values decided by the user
 
     # By default, it will consider all the first 2 props of all nested dicts of the properties.
