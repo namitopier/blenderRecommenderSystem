92be2e10da022af335d5cfb2061caf41822a0d53
namitopier <pierluigi0609@gmail.com>
2024-05-10 07:48:01 -0300

    TF-IDF algorithm working for the files starting with 'TUT'

---
+++ tf-idf.py
@@ -0,0 +1,103 @@
+# This script is meant to calculate the tf-idf of all tutorials of the recommender system
+import os
+import numpy as np
+
+# Tutorials list
+tutorials = []
+
+# Global list containing all the operations in the tutorials
+globalOps = []
+
+# Get the path to the folder containing the tutorials
+tutorials_path = os.path.join(os.path.dirname(__file__), 'blenderProject') 
+
+# List all files in the folder
+file_names = os.listdir(tutorials_path)
+
+# Filtering tutorials by initial letters: "TUT"
+tut_files = [file_name for file_name in file_names if file_name.startswith("TUT")]
+
+# Read content line by line for each file
+for i, file_name in enumerate(tut_files):
+    file_path = os.path.join(tutorials_path, file_name)
+    
+    # Dictionary containing all the information of current tutorial
+    tutDict = {"name" : file_name,
+               "operations": {},
+               "weights": []}
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            operationName = eval(line.strip())[0]
+
+            if operationName not in tutDict["operations"]:
+                tutDict["operations"][operationName] = 1
+            else:
+                tutDict["operations"][operationName] += 1
+
+            if operationName not in globalOps:
+                globalOps.append(operationName)
+    
+    tutorials.append(tutDict)
+
+
+
+# Number of tutorials considered
+N = len(tutorials)
+
+for tutorial in tutorials:
+    for term in globalOps:
+        if term in list(tutorial["operations"].keys()):
+             # Frequency of term in the current document
+            f = tutorial["operations"][term]
+
+            # Max frequency among all terms in the document
+            maxf = np.max(list(tutorial["operations"].values()))
+
+            # Number of tutorials where term occurs at least once
+            nk = 0
+            for tut in tutorials:
+                if term in list(tut["operations"].keys()):
+                    nk += 1
+            
+            # Calculating TF:
+            tf = f/maxf
+
+            # Calculating IDF:
+            idf = np.log10((N/nk))
+
+            # Calculating TF-IDF:
+            tf_idf = tf*idf
+
+        else:
+            tf_idf = 0
+
+        tutorial["weights"].append(tf_idf)
+
+    # Normalizing the weights:
+    tutorial["weights"] = np.divide(tutorial["weights"], np.sqrt(np.sum(np.square(tutorial["weights"]))))
+
+    # for term in list(tutorial["operations"].keys()):
+
+    #     # Frequency of term in the current document
+    #     f = tutorial["operations"][term]
+
+    #     # Max frequency among all terms in the document
+    #     maxf = np.max(list(tutorial["operations"].values()))
+
+    #     # Number of tutorials where term occurs at least once
+    #     nk = 0
+    #     for tut in tutorials:
+    #         if term in list(tut["operations"].keys()):
+    #             nk += 1
+        
+    #     # Calculating TF:
+    #     tf = f/maxf
+
+    #     # Calculating IDF:
+    #     idf = np.log10((N/nk))
+
+    #     # Calculating TF-IDF:
+    #     tf_idf = tf*idf
+
+print(tutorials)
